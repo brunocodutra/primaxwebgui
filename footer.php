9a557a66f89ba9a2b292ce896d8d4b4d4850b132
Bruno Dutra <brunocodutra@gmail.com>
2013-03-10 20:49:00 -0300

    gênesis

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="container">
+    	<hr>
+    	<div class="pull-right">
+			&copy; mosaic 2012
+		</div>
+    </div>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,17 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
+			<a class="brand" href="./index.html">mosaic</a>
+			<div class="nav-collapse collapse">
+				<form class="navbar-form pull-right">
+					<input class="span2" type="text" placeholder="username">
+					<input class="span2" type="password" placeholder="password">
+					<button type="submit" class="btn btn-primary">
+						Sign in
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
