78045f907aca3dfad475dd4c9ec7b18bad6248b7
Bruno Dutra <brunocodutra@gmail.com>
2013-03-17 16:44:24 -0300

    adding php module to control primax

---
+++ php/primax.php
@@ -0,0 +1,114 @@
+<?php
+    require_once('primax_constants.php');
+    
+    function tiff2png($tifFile) 
+    {
+        $magickWand = NewMagickWand();
+        MagickReadImage($magickWand, $tifFile);
+
+        if(MagickGetImageColorspace($magickWand) == MW_CMYKColorspace) 
+            MagickSetImageColorspace($magickWand, MW_RGBColorspace);
+
+        MagickSetImageFormat($magickWand, 'PNG' );
+        $pngFile = str_replace('.tif', '.png', $tifFile);
+        MagickWriteImage($magickWand, $pngFile);
+        
+        return $pngFile;
+    }
+
+    function turnLampOn($port)
+    {
+        exec("primaxscan --port='".$port."' --lamp=on", $output, $return);
+        if($return)
+            return -1;
+
+        global $warmUpTime;
+        return $warmUpTime;
+    }
+    
+    function turnLampOff($port)
+    {
+        exec("primaxscan --port='".$port."' --lamp=off", $output, $return);
+        if($return)
+            return -1;
+        
+        global $coolDownTime;
+        return $coolDownTime;
+    }
+        
+    function scan($port, $resolution, $x, $y, $dx, $dy, $speed, $contrast, $brightness, $gamma)
+    {
+        $resolution = min(max($resolution, $minResolution), $maxResolution);
+        $x = min(max($x, $minX), $maxX);
+        $y = min(max($y, $minY), $maxY);
+        $dx = min(max($dx, 0), $maxX - $x);
+        $dy = min(max($dy, 0), $maxY - $y);
+        $speed = min(max($speed, $minSpeed), $maxSpeed);
+        $contrast = min(max($contrast, $minContrast), $maxContrast);
+        $brightness = min(max($brightness, $minBrightness), $maxBrightness);
+        $gamma = min(max($gamma, $minGamma), $maxGamma);
+        $tifFile = '/tmp/primaxscan.tif';
+        
+        exec("primaxscan 
+            --port='".$port."' 
+            --speed=".$speed." 
+            --Scanner=".$maxResolution." 
+            --contrast=".$contrast." 
+            --brightness=".$brightness." 
+            --gamma=".$gamma." 
+            --Compression=none 
+            --RGB 
+            -p".$x."x".$y." 
+            -d".$dx."x".$dy." 
+            -r ".$resolution." 
+            -f ".$tifFile, $output, $return);
+            
+        if($return || !file_exists($tifFile))
+            $pngFile = '../img/broken_image.png';
+        else
+            $pngFile = tiff2png($tifFile);
+        
+        header("Content-type: image/png");
+        header("Expires: -1");
+        header("Cache-Control: no-store, no-cache, must-revalidate");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        readfile($pngFile);
+    }
+    
+    if(isset($_GET['op']))
+    {
+        $op = $_GET['op'];
+        $port = isset($_GET['port']) ? $_GET['port'] : $defaultPort;
+        
+        if($op === "turnLampOn")
+        {
+            echo turnLampOn($port);
+            exit();
+        }
+        else if($op === "turnLampOff")
+        {
+            echo turnLampOff($port);
+            exit();
+        }
+        else if($op === "scan" && isset($_GET['resolution']))
+        {
+            $resolution = $_GET['resolution'];
+            $x = isset($_GET['x']) ? $_GET['x'] : 0;
+            $y = isset($_GET['y']) ? $_GET['y'] : 0;
+            $dx = isset($_GET['dx']) ? $_GET['dx'] : $maxX - $x;
+            $dy = isset($_GET['dy']) ? $_GET['dy'] : $maxY - $y;
+            $speed = isset($_GET['speed']) ? $_GET['speed'] : $defaultSpeed;
+            $contrast = isset($_GET['contrast']) ? $_GET['contrast'] : $defaultContrast;
+            $brightness = isset($_GET['brightness']) ? $_GET['brightness'] : $defaultBrightness;
+            $gamma = isset($_GET['gamma']) ? $_GET['gamma'] : $defaultGamma;
+        
+            echo scan($port, $resolution, $x, $y, $dx, $dy, $speed, $contrast, $brightness, $gamma);
+            exit();
+        }
+    }
+    else
+    {
+        header("Location: ../404.html");
+        exit();
+    }
+?>
---
+++ php/primax_constants.php
@@ -0,0 +1,31 @@
+<?php   
+    $warmUpTime = 10;
+    $coolDownTime = 5;
+    
+    $minResolution = 10;
+    $maxResolution = 300;
+    
+    $minX = 0;
+    $maxX = 8.5;
+    
+    $minY = 0;
+    $maxY = 11.7;   
+    
+    $minSpeed = 0;
+    $maxSpeed = 15;
+    
+    $minContrast = -2000;
+    $maxContrast = 1000;
+    
+    $minBrightness = -200;
+    $maxBrightness = 200;
+    
+    $minGamma = 0.2;
+    $maxGamma = 5;
+    
+    $defaultPort = '0x378';    
+    $defaultSpeed = $maxSpeed;
+    $defaultContrast = 0;
+    $defaultBrightness = 0;
+    $defaultGamma = 1.0;
+?>
