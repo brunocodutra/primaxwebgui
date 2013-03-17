edcd8c55fad02b07fa3417584e95230353dc9316
Bruno Dutra <brunocodutra@gmail.com>
2013-03-17 19:10:31 -0300

    fixing author name in the footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <hr>
         <div class="pull-right">
-            &copy; bruno 2013
+            &copy; brunocodutra 2013
         </div>
     </div>
 </footer>
